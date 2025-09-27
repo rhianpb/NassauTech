9c70b34d010e3664494602393a590746ec4c4a25
Rhian Pablo <rhian.pablo16@gmail.com>
2025-09-27 18:48:26 -0300

    Upload do angular e spring

---
+++ nassautech-spring/src/main/java/nassautech_java/NassautechSpringApplication.java
@@ -0,0 +1,13 @@
+package nassautech_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NassautechSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NassautechSpringApplication.class, args);
+	}
+
+}
